f09b50fe9d128a182e6739e119b9da6b51a6e461
Elijah Agunbiade <45959516+ElijahAA@users.noreply.github.com>
2021-07-16 07:12:33 +1000

    Removed unnecessary app.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_avatars import Avatars
 from flask_login import LoginManager
 from flask_moment import Moment
 
